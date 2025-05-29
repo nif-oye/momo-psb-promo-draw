39f2357fb88a99af78b2d09cef0d9967fcd6c084
Oluwanifemi <oluwanifemi.dev@gmail.com>
2025-05-29 16:09:26 +0100

    Week 8

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const totalRounds = 11;
 
 // Utility Functions
 function loadPeopleFromCSV() {
-  fetch("week-7-players.csv")
+  fetch("week_8_referrers.csv")
     .then(res => res.text())
     .then(data => {
       const lines = data.split("\n").slice(1);
