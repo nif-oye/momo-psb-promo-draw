3894e430c07dbfa11c89e9f03fc050fcbdbae40a
Oluwanifemi <oluwanifemi.dev@gmail.com>
2025-06-26 15:48:27 +0100

    week-12

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const totalRounds = 11;
 
 // Utility Functions
 function loadPeopleFromCSV() {
-  fetch("week-11.csv")
+  fetch("week-12.csv")
     .then(res => res.text())
     .then(data => {
       const lines = data.split("\n").slice(1);
